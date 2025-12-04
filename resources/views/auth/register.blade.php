c4d73d7693c90737bcd0f9608a89393f09a8e7f3
telmoDev <telmodanielramirez@gmail.com>
2025-12-04 15:40:06 -0500

    Update registration form labels and add phone and address fields

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -10,27 +10,37 @@
         <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
             <form action="#" method="POST" class="space-y-6">
                   <div>
-                    <label for="name" class="block text-sm/6 font-medium text-gray-900">Nombre</label>
+                    <label for="name" class="block text-sm/6 font-medium text-gray-900">Nombres</label>
                     <div class="mt-2">
                         <input id="name" type="text" name="name" required autocomplete="name"
                             class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
                     </div>
                 </div>
                 <div>
-                    <label for="email" class="block text-sm/6 font-medium text-gray-900">Email</label>
+                    <label for="email" class="block text-sm/6 font-medium text-gray-900">Correo Electrónico</label>
                     <div class="mt-2">
                         <input id="email" type="email" name="email" required autocomplete="email"
+                            class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
+                    </div>
+                </div>
+                  <div>
+                    <label for="phone" class="block text-sm/6 font-medium text-gray-900">Telefono</label>
+                    <div class="mt-2">
+                        <input id="phone" type="text" name="phone" required autocomplete="phone"
+                            class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
+                    </div>
+                </div>
+                <div>
+                    <label for="address" class="block text-sm/6 font-medium text-gray-900">Dirección</label>
+                    <div class="mt-2">
+                        <input id="address" type="text" name="address" required autocomplete="address"
                             class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6" />
                     </div>
                 </div>
 
                 <div>
                     <div class="flex items-center justify-between">
-                        <label for="password" class="block text-sm/6 font-medium text-gray-900">Password</label>
-                        <div class="text-sm">
-                            <a href="#" class="font-semibold text-indigo-600 hover:text-indigo-500">Forgot
-                                password?</a>
-                        </div>
+                        <label for="password" class="block text-sm/6 font-medium text-gray-900">Contraseña</label>
                     </div>
                     <div class="mt-2">
                         <input id="password" type="password" name="password" required autocomplete="current-password"
